f16266c69472db400354b5ed40ec2c6d019933d7
S.V. Paulauskas <stanpaulauskas@gmail.com>
2018-03-07 19:48:55 -0500

    swapping out what file we're analyzing.

--- scan.bash
+++ scan.bash
@@ -7,7 +7,7 @@ exit
 fi
 
 prog=${HOME}/programs/laughing-conqueror/cmake-build-release/Analysis/Utkscan/utkscan
-data=${HOME}/data/utk/pixieworkshop/pulser_003.ldf
+data=${HOME}/data/ithemba/pr248/pr248-svp_003.ldf
 cfg=issue126.xml
 
 $prog -b -i $data -c $cfg -o $1

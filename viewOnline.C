1fd85a4eb68671f0355728551bae92debd8f4115
S.V. Paulauskas <stanpaulauskas@gmail.com>
2018-03-12 17:41:31 -0400

    Working version of the script

--- viewOnline.C
+++ viewOnline.C
@@ -1,19 +1,32 @@
 #include <TFile.h>
 #include <TH1.h>
+#include <TH2.h>
+#include <TH3.h>
 
 #include <iostream>
 
-void viewOnline(TFile *file, const int &id) {
+void viewOnline(const std::string &filename, const int &id) {
     const char *name = ("h"+std::to_string(id)).c_str();
-    TH1I *hist1 = nullptr;
+    TFile *file = TFile::Open(filename.c_str());
+    TH1D *hist1d = nullptr;
+    TH2D *hist2d = nullptr;
+    TH3D *hist3d = nullptr;
 
-    while(!hist1) {
-        file->ReadKeys();
-        delete file->FindObject(name);
-        file->GetObject(name, hist1);
-        //usleep(100000);
+    file->GetObject(name, hist1d);
+    if(hist1d)
+        hist1d->Draw();
+    else {
+        file->GetObject(name, hist2d);
+        if(hist2d)
+            hist2d->Draw("COLZ");
+        else {
+            file->GetObject(name, hist3d);
+            if(hist3d)
+                hist3d->Draw();
+            else
+                std::cout << "Couldn't figure out how to draw histogram with ID " << id << std::endl;
+        }
     }
-    hist1->Draw();
 }
 
 //void dd(unsigned int &id) {

f994c4e7942416f1c1125070a1ffe72a1b5dd7ef
S.V. Paulauskas <stanpaulauskas@gmail.com>
2018-03-15 17:37:57 -0400

    Adding and renaming forum examples

---
+++ example-28273.C
@@ -0,0 +1,60 @@
+#include <TFile.h>
+#include <TH1D.h>
+
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+void AsyncFlush(TFile *file, std::mutex *lock, unsigned int *loopId) {
+    std::cout << "AsyncFlush - Now calling file->Write" << std::endl;
+    file->Write(0, TObject::kWriteDelete);
+    std::cout << "AsyncFlush - Now unlocking the file for writing in loop " << *loopId << std::endl;
+    lock->unlock();
+}
+
+void WriteToDisk(TFile *file, unsigned int *loopID, std::mutex *lock) {
+    if (lock->try_lock()) {
+        std::cout << "WriteToDisk - we're creating the new thread in loop number " << *loopID << endl;
+        std::thread worker0(AsyncFlush, file, lock, loopID);
+        worker0.detach();
+    }
+}
+
+void GenerateHistogram() {
+    std::mutex lock;
+    TFile *f = new TFile("test.root", "RECREATE");
+    TH1D *hist = new TH1D("hist", "", 100, -2, 2);
+    unsigned int loopCounter = 0;
+    while (loopCounter < 40000) {
+        hist->FillRandom("gaus", 10000);
+        WriteToDisk(f, &loopCounter, &lock);
+        loopCounter++;
+    }
+    while(!lock.try_lock())
+        sleep(1);
+    f->Write(nullptr, TObject::kWriteDelete);
+    f->Close();
+}
+
+// Adapted from
+// https://root-forum.cern.ch/t/read-a-tfile-while-writing-with-another-process/18608/6
+// and follow up comment
+// https://root-forum.cern.ch/t/read-histogram-tree-from-tfile-while-actively-writing-to-it/28273/10
+void ReadHistogram(TFile *file, const char *name = "hist") {
+    TH1D *hist1 = nullptr;
+    delete file->FindObject(name);
+    file->ReadKeys();
+    file->GetObject(name, hist1);
+    if(hist1)
+       hist1->Draw();
+}
+
+// Adapted from
+// https://root-forum.cern.ch/t/read-a-tfile-while-writing-with-another-process/18608/7
+void ReadHistogram() {
+    TFile *file = TFile::Open("test.root");
+    TH1D *hist = nullptr;
+    file->GetObject("hist", hist);
+    if(hist)
+        hist->Draw();
+}
--- viewOnline.C
+++ viewOnline.C
@@ -5,9 +5,15 @@
 
 #include <iostream>
 
-void viewOnline(const std::string &filename, const int &id) {
+// Adapted from
+// https://root-forum.cern.ch/t/read-a-tfile-while-writing-with-another-process/18608/6
+// and follow up comment
+// https://root-forum.cern.ch/t/read-histogram-tree-from-tfile-while-actively-writing-to-it/28273/10
+void ReadHistogram(TFile *file, const unsigned int &id) {
     const char *name = ("h"+std::to_string(id)).c_str();
-    TFile *file = TFile::Open(filename.c_str());
+    delete file->FindObject(name);
+    file->ReadKeys();
+
     TH1D *hist1d = nullptr;
     TH2D *hist2d = nullptr;
     TH3D *hist3d = nullptr;
@@ -29,10 +35,29 @@ void viewOnline(const std::string &filename, const int &id) {
     }
 }
 
-//void dd(unsigned int &id) {
-//    _file0->ReadKeys();
-//    delete f->FindObject(name);
-//    TH2I *h2;
-//    f->GetObject(name, h);
-//    h->Draw();
-//}
+// Adapted from
+// https://root-forum.cern.ch/t/read-a-tfile-while-writing-with-another-process/18608/7
+void ReadHistogram(const std::string &filename, const int &id) {
+    const char *name = ("h"+std::to_string(id)).c_str();
+    TFile *file = TFile::Open(filename.c_str());
+
+    TH1D *hist1d = nullptr;
+    TH2D *hist2d = nullptr;
+    TH3D *hist3d = nullptr;
+
+    file->GetObject(name, hist1d);
+    if(hist1d)
+        hist1d->Draw();
+    else {
+        file->GetObject(name, hist2d);
+        if(hist2d)
+            hist2d->Draw("COLZ");
+        else {
+            file->GetObject(name, hist3d);
+            if(hist3d)
+                hist3d->Draw();
+            else
+                std::cout << "Couldn't figure out how to draw histogram with ID " << id << std::endl;
+        }
+    }
+}
